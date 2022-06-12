69bc2408ff8039fba10f6cd75302732f2dddc655
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-06-12 20:31:47 +0800

    Initial Commit

---
+++ SEB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SEB
+//
+//  Created by Jia Chen Yee on 12/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SEB/SEBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SEBApp.swift
+//  SEB
+//
+//  Created by Jia Chen Yee on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SEBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
