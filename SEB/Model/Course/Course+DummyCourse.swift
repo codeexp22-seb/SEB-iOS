201636050c19802e17be7cad33155e1ec76fa890
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-06-14 01:07:32 +0800

    add dummy course

---
+++ SEB/Model/Course/Course+DummyCourse.swift
@@ -0,0 +1,14 @@
+//
+//  Course+DummyCourse.swift
+//  SEB
+//
+//  Created by Jia Chen Yee on 14/6/22.
+//
+
+import Foundation
+
+extension Course {
+    #warning("@Rui Yang please add your courses here ❤️")
+    static let dummy: [Course] = [
+    ]
+}
